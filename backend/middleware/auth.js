68216fbceebfbc55c92296aea378c84bb101a70b
riffca <riffca@yandex.ru>
2016-06-26 17:59:06 +0300

    fix auth problrm with promise

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,34 +1,23 @@
 "use strict";
-let User = require('../model/user');
 
 let jwt = require('jsonwebtoken');
 let sercretKey = require('../config').secretKey;
 
-class Auth {
-    constructor(){
-        console.log('Class Auth created');
-    }
-    createToken(req,res,next,user){
-        jwt.sign({
-            id: user._id,
-            name: user.name,
-            email: user.email
-        }, sercretKey, {
-            expiresIn: '10h'
-        }, (err, token) => {
-            if (err) return err;
-            res.newToken = token;
-            res.newUser = user;
-            next();
+module.exports =  {
+    createToken(user) {
+        return new Promise((resolve, reject) => {
+            jwt.sign({
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }, sercretKey, {
+                expiresIn: '10h'
+            }, (err, token) => {
+                if (err) reject(err);
+                resolve(token);
+            });
         });
-    }
-    createNewUser(req, res, next) {
-        let user = new User({
-            email: req.body.email,
-            password: req.body.password
-        });
-        this.createToken(req,res, next, user);  
-    }
+    },
     authCheck(req, res, next) {
         let authToken = 'secret';
         let token = req.headers['x-access-token'];
@@ -47,13 +36,7 @@ class Auth {
             res.authStatus = 'user';
             next();
         }
-
     }
-}
+};
 
 
-
-
-   
-
-module.exports = new Auth();
--- backend/model/user.api.js
+++ backend/model/user.api.js
@@ -1,68 +1,73 @@
 "use strict";
 let User = require('./user');
-
-//A P I
 let auth = require('../middleware/auth');
-
+//A P I
 module.exports = function(express) {
     let api = express.Router();
     //create user
-    api.post('/signup', auth.createNewUser,(req, res) => {
-        res.newUser.save((err) => {
-            if (err) {
-                res.send(err);
-                return;
-            }
-            res.json({
-                success: true,
-                message: "New user has been created",
-                token: res.newToken
-            });
+    api.post('/signup', (req, res) => {
+        let user = new User({
+            email: req.body.email,
+            password: req.body.password
         });
+        auth.createToken(user)
+            .then(token => {
+                user.save(err => {
+                    if (err) {
+                        res.send(err);
+                        return;
+                    }
+                    res.json({
+                        success: true,
+                        message: "New user has been created",
+                        token: token
+                    });
+                });
+            });
     });
     //login user
     api.post('/login', (req, res) => {
         User
-        .findOne({
-            username: req.body.email
-        })
-        .select('name password')
-        .exec((err, user)=>{
-            if (err) throw err;
-            if (!user) {
-                res.send({ message: "Некорректные данные" });
-            } else if (user) {
-                var validPassword = user.comparePassword(req.body.password);
-                if (!validPassword) {
-                    res.send({ message: "Неверный пароль" });
-                } else {
-                    // token
-                    let token = auth.createToken(req,res,user);
-                    res.json({
-                        success: true,
-                        message: "Successfully login",
-                        token: token
-                    });
+            .findOne({
+                username: req.body.email
+            })
+            .select('name password')
+            .exec((err, user) => {
+                if (err) throw err;
+                if (!user) {
+                    res.send({ message: "Некорректные данные" });
+                } else if (user) {
+                    var validPassword = user.comparePassword(req.body.password);
+                    if (!validPassword) {
+                        res.send({ message: "Неверный пароль" });
+                    } else {
+                        auth.createToken(user).then(token => {
+                            res.json({
+                                success: true,
+                                message: "Successfully login",
+                                token: token
+                            });
+                        });
+                    }
                 }
-            }
+            });
+    });
+    api.get('/user', auth.authCheck, (req, res) => {
+        res.json({
+            id: 777,
+            name: 'stas',
+            email: 'riffca@ya.ru',
+            phoneNumber: '',
+            login: true,
+            basket: {
+                users: [{
+                    name: 'step'
+                }, {
+                    name: 'ann'
+                }]
+            },
+            status: res.authStatus
         });
     });
-    // api.get('/user', authCheck, (req, res) => {
-    //     res.json({
-    //         id: 777,
-    //         name: 'stas',
-    //         email: 'riffca@ya.ru',
-    //         phoneNumber: '',
-    //         login: true,
-    //         basket: {
-    //             users: [{
-    //                 name: 'step'
-    //             }, {
-    //                 name: 'ann'
-    //             }]
-    //         },
-    //         status: res.authStatus
-    //     });
-    // });
     return api;
 };
