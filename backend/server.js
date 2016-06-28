036b4f70697da9bc6c465adfdf18ebb4a3b401c5
riffca <riffca@yandex.ru>
2016-06-28 05:45:46 +0300

    learn git branches with git checkout -b and git merge

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-//let config = require('./config');
+let config = require('./config');
 app.listen(config.port, () => {
     console.log('server started at port ' + config.port);
 });
